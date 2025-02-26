0f941cd510ee5c473c28b1ac4f133d5c7524f07e
Vishwa <135529226+vishwaDamruth@users.noreply.github.com>
2025-02-26 19:09:50 +0530

    start, restart and game logic implemented

--- game.js
+++ game.js
@@ -1,3 +1,15 @@
+const displayController = (() => {
+    const renderMessage = (message) => {
+        document.querySelector("#message").innerHTML = message;   
+     }
+     return{
+        renderMessage,
+     }
+})();
+
+
+
+
 const Gameboard = (() => {
     let gameboard = ["","","","","","","","",""]
 
@@ -20,11 +32,14 @@ const Gameboard = (() => {
     const update = (index,value) => {
         gameboard[index] = value;
         render();
-    }
+    };
+
+    const getGameboard = () => gameboard;
 
     return{
         render,
-        update
+        update,
+        getGameboard,
     }
 
 })();
@@ -63,20 +78,76 @@ const Game = (() =>{
     }
 
     const handleClick = (event) => {
+
+        if(gameOver){
+            return;
+        }
         let index = parseInt(event.target.id.split("-")[1]);
-        Gameboard.update(index, players[currentPlayerIndex].mark)
+        
     
-    currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
+
+        if(Gameboard.getGameboard()[index] !== "")
+            return;
+        Gameboard.update(index, players[currentPlayerIndex].mark);
+    
+        if(checkForWin(Gameboard.getGameboard(),players[currentPlayerIndex].mark)){
+            gameOver = true;
+            displayController.renderMessage(`${players[currentPlayerIndex].name} wins!`);
+        } else if(checkForTie(Gameboard.getGameboard())){
+            gameOver = true;
+            displayController.renderMessage("It's a Tie!");
+        }
+        currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
     }
 
 
+    const restart = () => {
+        for (let i = 0;i<9;i++){
+            Gameboard.update(i,"")
+        }
+        Gameboard.render();
+    }
+
     return{
         start,
+        restart,
         handleClick
     }
  })();
 
+function checkForWin(board){
+    const winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5 ],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
+    for(let i = 0; i< winningCombinations.length; i++){
+        const[a,b,c] = winningCombinations[i];
+        if(board[a] && board[a] === board[b] && board[a] === board[c]){
+                return true;
+        }
+    }
+    return false;
+}
 
+
+
+
+function checkForTie(board) {
+    return board.every(cell => cell !== "")
+}
+
+
+
+const restartButton = document.querySelector("#restart-button");
+restartButton.addEventListener("click",() => {
+    Game.restart();
+})
 
 
 
