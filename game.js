98a63bf6f5da57d7637aeb38348d1f81e5e5fc3b
Vishwa <135529226+vishwaDamruth@users.noreply.github.com>
2025-02-26 15:57:25 +0530

    Tic Tac Toe layout has been created. Can now update every square using a click with X or O

---
+++ game.js
@@ -0,0 +1,87 @@
+const Gameboard = (() => {
+    let gameboard = ["","","","","","","","",""]
+
+    const render = () => {
+        let boardHTML = "";
+        gameboard.forEach((square, index) => {
+            boardHTML += `<div class = "square" id = "square-${index}">${square}</div>`
+
+        })
+        document.querySelector("#gameboard").innerHTML = boardHTML;
+
+        const squares = document.querySelectorAll(".square");
+        squares.forEach((square) => {
+            square.addEventListener("click",Game.handleClick);
+        })
+
+   
+    }
+
+    const update = (index,value) => {
+        gameboard[index] = value;
+        render();
+    }
+
+    return{
+        render,
+        update
+    }
+
+})();
+
+
+const createPlayer = (name, mark) => {
+    return{
+        name,
+        mark
+    }
+}
+
+const Game = (() =>{
+    let players = [];
+    let currentPlayerIndex;
+    let gameOver;
+
+    const start = () => {
+        players = [
+            createPlayer(document.querySelector("#player1").value, "X"),
+            createPlayer(document.querySelector("#player2").value, "O")
+        ]
+        
+        currentPlayerIndex = 0;
+        gameOver = false;
+        Gameboard.render();
+        const squares = document.querySelectorAll(".square");
+        squares.forEach((square) => {
+            square.addEventListener("click",handleClick);
+        })
+
+       
+
+
+
+    }
+
+    const handleClick = (event) => {
+        let index = parseInt(event.target.id.split("-")[1]);
+        Gameboard.update(index, players[currentPlayerIndex].mark)
+    
+    currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
+    }
+
+
+    return{
+        start,
+        handleClick
+    }
+ })();
+
+
+
+
+
+
+const startButton = document.querySelector("#start-button");
+startButton.addEventListener("click", () => {
+    Game.start();
+})
